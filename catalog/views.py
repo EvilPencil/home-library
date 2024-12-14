21eddd82625bfeded621a60806bd90cfba9cb2eb
Home Library <django@d3.home.arpa>
2024-12-14 23:34:24 +0300

    Autor list,detail

--- catalog/views.py
+++ catalog/views.py
@@ -10,10 +10,17 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 2
 
 class BookDetailView(generic.DetailView):
     model = Book
     
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
 
 def catalog(request):
     num_books=Book.objects.all().count()
